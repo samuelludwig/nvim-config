7481abcd013106f7ef03fe5dc0b75a3f0120f5f6
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-07-12 09:23:09 -0400

    Refactor some plugin options into lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,66 +8,60 @@ return require('packer').startup(function(use)
   }
 
   use {
-      'ayu-theme/ayu-vim',
-      config = function()
-          vim.cmd(
-          [[
-          let ayucolor="light"
-          "let ayucolor="mirage"
-          "let ayucolor="dark"
-          ]])
-      end
+    'ayu-theme/ayu-vim',
+    config = function()
+      vim.cmd(
+      [[
+      let ayucolor="light"
+      "let ayucolor="mirage"
+      "let ayucolor="dark"
+      ]])
+    end
   }
   use { 'sts10/vim-pink-moon' }
   use { 'arcticicestudio/nord-vim' }
   use { 'kamwitsta/flatwhite-vim' }
   use { 'rktjmp/lush.nvim' }
   use {
-      'joshdick/onedark.vim',
-      config = function()
-          vim.cmd(
-          [[
-          "Hide ~'s
-          let g:onedark_hide_endofbuffer = 1
+    'joshdick/onedark.vim',
+    config = function()
+      --Hide ~'s
+      vim.g.onedark_hide_endofbuffer = 1
 
-          "Enable italics
-          let g:onedark_terminal_italics = 1
-          ]])
-      end
+      --Enable italics
+      vim.g.onedark_terminal_italics = 1
+    end
   }
   use {
-      'overcache/NeoSolarized',
-      config = function()
-          vim.cmd(
-          [[
-          " Default value is "normal", Setting this option to "high" or "low" does
-          " use the same Solarized palette but simply shifts some values up or
-          " down in order to expand or compress the tonal range displayed.
-          let g:neosolarized_contrast = "normal"
+    'overcache/NeoSolarized',
+    config = function()
+      -- Default value is "normal", Setting this option to "high" or "low" does
+      -- use the same Solarized palette but simply shifts some values up or
+      -- down in order to expand or compress the tonal range displayed.
+      vim.g.neosolarized_contrast = "normal"
 
-          " Special characters such as trailing whitespace, tabs, newlines, when
-          " displayed using ":set list" can be set to one of three levels
-          " depending on your needs.  Default value is "normal". Provide "high"
-          " and "low" options.
-          let g:neosolarized_visibility = "normal"
+      -- Special characters such as trailing whitespace, tabs, newlines, when
+      -- displayed using ":set list" can be set to one of three levels
+      -- depending on your needs.  Default value is "normal". Provide "high"
+      -- and "low" options.
+      vim.g.neosolarized_visibility = "normal"
 
-          " I make vertSplitBar a transparent background color. If you like the
-          " origin solarized vertSplitBar style more, set this value to 0.
-          let g:neosolarized_vertSplitBgTrans = 1
+      -- I make vertSplitBar a transparent background color. If you like the
+      -- origin solarized vertSplitBar style more, set this value to 0.
+      vim.g.neosolarized_vertSplitBgTrans = 1
 
-          " If you wish to enable/disable NeoSolarized from displaying bold,
-          " underlined or italicized" typefaces, simply assign 1 or 0 to the
-          " appropriate variable.  Default values:
-          let g:neosolarized_bold = 1
-          let g:neosolarized_underline = 1
-          let g:neosolarized_italic = 0
+      -- If you wish to enable/disable NeoSolarized from displaying bold,
+      -- underlined or italicized" typefaces, simply assign 1 or 0 to the
+      -- appropriate variable.  Default values:
+      vim.g.neosolarized_bold = 1
+      vim.g.neosolarized_underline = 1
+      vim.g.neosolarized_italic = 0
 
-          " Used to enable/disable "bold as bright" in Neovim terminal. If colors
-          " of bold text output by commands like `ls` aren't what you expect, you
-          " might want to try disabling this option. Default value:
-          let g:neosolarized_termBoldAsBright = 1
-          ]])
-      end
+      -- Used to enable/disable "bold as bright" in Neovim terminal. If colors
+      -- of bold text output by commands like `ls` aren't what you expect, you
+      -- might want to try disabling this option. Default value:
+      vim.g.neosolarized_termBoldAsBright = 1
+    end
   }
 
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
@@ -77,23 +71,18 @@ return require('packer').startup(function(use)
   use {
     'SirVer/ultisnips',
     config = function()
-      vim.cmd(
-      [[
-      " Trigger configuration. You need to change this to something else
-      " than <tab> if you use https://github.com/Valloric/YouCompleteMe.
-      let g:UltiSnipsExpandTrigger="<tab>"
-      let g:UltiSnipsJumpForwardTrigger="<c-s>"
-      let g:UltiSnipsJumpBackwardTrigger="<c-a>"
-      ]]) -- We dont need any return values
+      -- Trigger configuration. You need to change this to something else
+      -- than <tab> if you use https://github.com/Valloric/YouCompleteMe.
+      vim.g.UltiSnipsExpandTrigger="<tab>"
+      vim.g.UltiSnipsJumpForwardTrigger="<c-s>"
+      vim.g.UltiSnipsJumpBackwardTrigger="<c-a>"
     end
   }
   use { 'honza/vim-snippets' }
 
   use {
     'nvim-telescope/telescope.nvim',
-    requires = {
-      'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim'
-    }
+    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' }
   }
 
   use {
