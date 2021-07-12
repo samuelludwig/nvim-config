208bcde799fd0aa5a2afdbce2990b51ac141fc83
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-07-12 15:05:07 -0400

    Refactor and add which-key and aerial.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,16 +22,7 @@ return require('packer').startup(function(use)
   use { 'arcticicestudio/nord-vim' }
   use { 'kamwitsta/flatwhite-vim' }
   use { 'rktjmp/lush.nvim' }
-  use {
-    'joshdick/onedark.vim',
-    config = function()
-      --Hide ~'s
-      vim.g.onedark_hide_endofbuffer = 1
-
-      --Enable italics
-      vim.g.onedark_terminal_italics = 1
-    end
-  }
+  use { 'navarasu/onedark.nvim' }
   use {
     'overcache/NeoSolarized',
     config = function()
@@ -82,7 +73,17 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' }
+    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
+    config = function()
+      local bind_telescope_cmd = function(bind, command)
+        cmd_string = [[<cmd>Telescope ]] .. command .. [[<cr>]]
+        return vim.api.nvim_set_keymap('n', bind, cmd_string, { noremap = true })
+      end
+      bind_telescope_cmd('<leader>ff', 'find_files')
+      bind_telescope_cmd('<leader>fg', 'live_grep')
+      bind_telescope_cmd('<leader>fb', 'buffers')
+      bind_telescope_cmd('<leader>fh', 'help_tags')
+    end
   }
 
   use {
@@ -172,4 +173,71 @@ return require('packer').startup(function(use)
     requires = 'kyazdani42/nvim-web-devicons',
     config = function() require('configs/plugins/nvim-tree') end
   }
+
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+        plugins = {
+          marks = true, -- shows a list of your marks on ' and `
+          registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+          -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+          -- No actual key bindings are created
+          spelling = {
+            enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+            suggestions = 20, -- how many suggestions should be shown in the list?
+          },
+          presets = {
+            operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+            motions = true, -- adds help for motions
+            text_objects = true, -- help for text objects triggered after entering an operator
+            windows = true, -- default bindings on <c-w>
+            nav = true, -- misc bindings to work with windows
+            z = true, -- bindings for folds, spelling and others prefixed with z
+            g = true, -- bindings for prefixed with g
+          },
+        },
+        -- add operators that will trigger motion and text object completion
+        -- to enable all native operators, set the preset / operators plugin above
+        operators = { gc = "Comments" },
+        key_labels = {
+          -- override the label used to display some keys. It doesn't effect WK in any other way.
+          -- For example:
+          -- ["<space>"] = "SPC",
+          -- ["<cr>"] = "RET",
+          -- ["<tab>"] = "TAB",
+        },
+        icons = {
+          breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+          separator = "➜", -- symbol used between a key and it's label
+          group = "+", -- symbol prepended to a group
+        },
+        window = {
+          border = "none", -- none, single, double, shadow
+          position = "bottom", -- bottom, top
+          margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+          padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+        },
+        layout = {
+          height = { min = 4, max = 25 }, -- min and max height of the columns
+          width = { min = 20, max = 50 }, -- min and max width of the columns
+          spacing = 3, -- spacing between columns
+          align = "left", -- align columns left, center or right
+        },
+        ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+        hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
+        show_help = true, -- show help message on the command line when the popup is visible
+        triggers = "auto", -- automatically setup triggers
+        -- triggers = {"<leader>"} -- or specify a list manually
+        triggers_blacklist = {
+          -- list of mode / prefixes that should never be hooked by WhichKey
+          -- this is mostly relevant for key maps that start with a native binding
+          -- most people should not need to change this
+          i = { "j", "k" },
+          v = { "j", "k" },
+        },
+      }
+    end
+  }
+
 end)
