9fb42126b739f5ba12282fc21b84b2651f4f2c19
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-07-14 11:18:40 -0400

    Actually enable treesitter modules properly

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -55,7 +55,39 @@ return require('packer').startup(function(use)
     end
   }
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        --ignore_install = { "javascript" }, -- List of parsers to ignore installing
+        highlight = {
+          enable = true,              -- false will disable the whole extension
+          --disable = { "c", "rust" },  -- list of language that will be disabled
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      }
+      vim.cmd(
+      [[
+      set foldmethod=expr
+      set foldexpr=nvim_treesitter#foldexpr()
+      ]])
+    end
+  }
 
   use { 'b3nj5m1n/kommentary' }
 
